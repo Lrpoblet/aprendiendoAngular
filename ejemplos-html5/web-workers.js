21ddb4a143e30c26fcc56acb74a6d561444ccd87
Alicia Daza <leyamcir@gmail.com>
2016-05-30 23:08:09 +0200

    Web Workers Countdown

---
+++ ejemplos-html5/web-workers.js
@@ -0,0 +1,11 @@
+function countDown(from) {
+    // Notify main thread
+    postMessage(from);
+
+    // Chech that countDown isn't arrived to 0
+    if (from > 0){
+        setTimeout(function () {
+            countDown(--from); // Executes -1 before calling countDown function
+        }, 1000);
+    }
+}
